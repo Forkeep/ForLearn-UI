841f3c0fced08acc8a929507d342768302405672
Forkeep <13126593533@163.com>
2020-05-05 14:29:42 +0800

    添加Input组件

---
+++ src/input.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <label>
+      <input :value="value" :disabled="disabled" :readonly="readonly">
+    </label>
+    <template v-if="error">
+      <icon name="error" class="icon-error"></icon>
+      <span class="errorMessage">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+  import Icon from './icon'
+
+  export default {
+    components: {Icon},
+    name: 'FLInput',
+    props: {
+      value: {
+        type: String
+      },
+      error: {
+        type: String
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      },
+      readonly: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  $height: 32px;
+  $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 12px;
+  $box-shadow-color: rgba(0, 0, 0, 0.5);
+  $red: #F1453D;
+  .wrapper {font-size: $font-size;display: inline-flex;align-items: center;justify-content: center;
+
+     input {height: $height;border: 1px solid $border-color;border-radius: $border-radius;padding: 0 8px;font-size: inherit;
+
+      &:hover {border-color: $border-color-hover;}
+
+      &:focus {box-shadow: inset 0 1px 3px $box-shadow-color;outline: none;}
+
+      &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed;}
+    }
+
+    > :not(:last-child) {margin-right: .5em;}
+
+    &.error {
+       input {border-color: $red;}
+    }
+
+    .icon-error {fill: $red;}
+
+    .errorMessage {color: $red;}
+
+  }
+</style>
