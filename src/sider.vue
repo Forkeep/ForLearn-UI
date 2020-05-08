ab1f35be3a9220cd5d1f50321519edca1ff0ebba
Forkeep <13126593533@163.com>
2020-05-08 11:30:00 +0800

    给slider增加滑动动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="sider">
-    <slot/>
-  </div>
+  <transition name="slide">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=false" class="close">关闭</button>
+    </div>
+  </transition>
 </template>
 
 <script lang="js">
   export default {
-    name: 'FLSider'
+    name: 'FLSider',
+    data() {
+      return {
+        visible: true
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .sider{
+    position: relative;
+    .close{
+      position: absolute;
+      right: 0;
+      top:0;
+    }
+  }
+  .slide-enter-active, .slide-leave-active {
+    transition: all .5s;
+  }
 
+  .slide-enter, .slide-leave-to /* .fade-leave-active below version 2.1.8 */
+  {
+    margin-left: -200px;
+  }
 </style>
